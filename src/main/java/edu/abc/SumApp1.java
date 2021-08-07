bb29297a9c318374c67745735a4897097fe62f7e
Uddhav <uddhavjambhalkar26@gmail.com>
2021-08-07 17:35:11 +0530

    gradle project1

---
+++ src/main/java/edu/abc/SumApp1.java
@@ -0,0 +1,15 @@
+package edu.abc;
+
+public class SumApp1 {
+	
+	public static void main(String[] args) {
+		int sum=0;
+		
+		for (int i = 0; i < 11; i++) {
+			sum = sum +i;
+		}
+		
+		System.out.println("Sum is "+sum);
+	}
+
+}
